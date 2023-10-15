e6cf67400e548070406c94c1bb110ea9f0707f65
ssadras <sadra3tarehdan@gmail.com>
2023-10-15 05:14:50 -0400

    demo debugging

--- proxy/distributed-server-controller.py
+++ proxy/distributed-server-controller.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 from ping3 import ping
+import threading
 
 all_servers = []
 ready_server = None
@@ -9,28 +10,37 @@ server_app = Flask(__name__)
 
 @server_app.route('/api/server_init', methods=['POST'])
 def receive_data():
-    data = request.get_json()
+    data = request.json
     all_servers.append(data['ip'])
-    ping_server(data)
-    return 'Success!', 200
+    print(data)
+
+    ping_thread = threading.Thread(target=ping_server, args=(data['ip'],))
+    return jsonify({'docker_url': 'Success!'}), 200
 
 
 @server_app.route('/DeServer')
 def redirect_user():
+    global ready_server
+
+    print(ready_server)
     if ready_server != None:
         return redirect(ready_server)
     # Loop them until a valid server appears if no server appeared initially, this is only nessecary for the demo
-    # as redundancy is less resiliant
-    return redirect(url_for('DeServer'))
+    # as redundancy is less resilient
+    return redirect(url_for('redirect_user'))
 
 
 def ping_server(server: str):
-    time = ping(server)
-    all_servers.remove(server)
-    if time != None:
-        all_servers.insert(server, 0)
-        ready_server = server
-        ping_server(server)
+    global ready_server
+
+    while True:
+        time = ping(server)
+        all_servers.remove(server)
+        if time != None:
+            all_servers.insert(0, server)
+            ready_server = server
+        else:
+            break
 
 
-server_app.run(debug=True, port=8000)
+server_app.run(debug=True, port=8000, host="0.0.0.0")
