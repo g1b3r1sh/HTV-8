063427213c43db3757eca469aa5d0ba1191fe4ac
ssadras <sadra3tarehdan@gmail.com>
2023-10-15 05:30:04 -0400

    redirection debugged

--- proxy/distributed-server-controller.py
+++ proxy/distributed-server-controller.py
@@ -13,8 +13,10 @@ def receive_data():
     data = request.json
     all_servers.append(data['ip'])
     print(data)
+    print(ping(data['ip']))
 
     ping_thread = threading.Thread(target=ping_server, args=(data['ip'],))
+    ping_thread.start()
     return jsonify({'docker_url': 'Success!'}), 200
 
 
@@ -22,9 +24,8 @@ def receive_data():
 def redirect_user():
     global ready_server
 
-    print(ready_server)
     if ready_server != None:
-        return redirect(ready_server)
+        return redirect(f"http://{ready_server}:8080" , code=302)
     # Loop them until a valid server appears if no server appeared initially, this is only nessecary for the demo
     # as redundancy is less resilient
     return redirect(url_for('redirect_user'))
