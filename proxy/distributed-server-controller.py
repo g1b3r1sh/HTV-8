a61a3094b6e10c498c22fdbf07780b502dbd2417
ssadras <sadra3tarehdan@gmail.com>
2023-10-15 04:09:53 -0400

    code styling corrected

--- proxy/distributed-server-controller.py
+++ proxy/distributed-server-controller.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, redirect, url_for
 from ping3 import ping
 
-
 all_servers = []
 ready_server = None
 
 server_app = Flask(__name__)
+
 
 @server_app.route('/api/server_init', methods=['POST'])
 def receive_data():
@@ -13,6 +13,7 @@ def receive_data():
     all_servers.append(data['ip'])
     ping_server(data)
     return 'Success!', 200
+
 
 @server_app.route('/DeServer')
 def redirect_user():
@@ -22,6 +23,7 @@ def redirect_user():
     # as redundancy is less resiliant
     return redirect(url_for('DeServer'))
 
+
 def ping_server(server: str):
     time = ping(server)
     all_servers.remove(server)
@@ -29,5 +31,6 @@ def ping_server(server: str):
         all_servers.insert(server, 0)
         ready_server = server
         ping_server(server)
+
 
 server_app.run(debug=True, port=8000)
