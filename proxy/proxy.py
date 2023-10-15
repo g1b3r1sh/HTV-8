f1a57f1b3ac9a1227b4115b578487746c7c03b1c
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2023-10-15 01:41:03 -0400

    Add more features to order page

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -34,19 +34,40 @@ sign_in_md = Markdown("""
 
 """)
 
-docker_image = None
+
+def generate_ram_size_strings():
+    ram_sizes = []
+    for i in range(128, 1000, 128):
+        ram_sizes.append(f"{i} MB")
+    for i in range(1, 17):
+        ram_sizes.append(f"{i} GB")
+    return ram_sizes
+ram_sizes = generate_ram_size_strings()
+
 max_ping = 100
+num_threads = 1
+ram = "128 MB"
+docker_image = None
 
 dashboard_md = Markdown("""
-# Dashboard
+# Order Server
 
 <|{max_ping}|number|label=Max Ping (ms)|>
 
 
-<|{threads}|slider|min=1|max=128|value=1000|>
+Threads:
+
+<|{num_threads}|slider|min=1|max=128|>
+
+RAM:
+
+<|{ram}|slider|lov={ram_sizes}|text_anchor=none|>
 
 
-<|{docker_image}|file_selector|label=Upload Docker Image|extensions=*|on_action=on_upload|>
+<|{docker_image}|file_selector|label=Upload Docker Image|extensions=.tar|on_action=on_upload|>
+
+
+<|Order|button|on_action=on_order_server|>
 
 """)
 
@@ -72,6 +93,11 @@ def on_upload(state):
     dir(state)
     with open(state.docker_image, 'rb') as f:
         r = requests.post(url, data={'pxeconfig': f.read()})
+
+def on_order_server(state):
+    print(state.max_ping)
+    print(state.num_threads)
+    print(state.ram)
 
 def get_lan_ip() -> str:
     iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
